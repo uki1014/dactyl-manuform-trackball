736d9332002a05625f26c0967bec506d414c627a
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-01-15 15:20:49 -0500

    Turned off debug.

--- keyboards/handwired/wylderbuilds/trackball/trackball.c
+++ keyboards/handwired/wylderbuilds/trackball/trackball.c
@@ -202,20 +202,15 @@ static void pointing_device_task_charybdis(report_mouse_t* mouse_report) {
 #    else
         scroll_buffer_y += mouse_report->y;
 #    endif  // CHARYBDIS_DRAGSCROLL_REVERSE_Y
-//        if (scroll_buffer_x != 0 || scroll_buffer_y != 0) {
-//            printf("trackball.c: scroll_buffer_x: %d, scroll_buffer_y: %d\n", scroll_buffer_x, scroll_buffer_y);
-//        }
         mouse_report->x = 0;
         mouse_report->y = 0;
         if (abs(scroll_buffer_x) > CHARYBDIS_DRAGSCROLL_BUFFER_SIZE) {
             mouse_report->h = scroll_buffer_x > 0 ? 1 : -1;
             scroll_buffer_x = 0;
-            printf("h changed by %d'n", mouse_report->h);
         }
         if (abs(scroll_buffer_y) > CHARYBDIS_DRAGSCROLL_BUFFER_SIZE) {
             mouse_report->v = scroll_buffer_y > 0 ? 1 : -1;
             scroll_buffer_y = 0;
-            printf("v changed by %d\n", mouse_report->v);
         }
 
     } else if (!g_charybdis_config.is_sniping_enabled) {

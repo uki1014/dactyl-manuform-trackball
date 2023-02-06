126314363231299dedb5d5bd6c80b1fa15038bd4
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-02-06 12:41:36 -0500

    Working for peripro-303

--- keyboards/handwired/wylderbuilds/trackball/4x6_3_all_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/4x6_3_all_track/keymaps/vial/keymap.c
@@ -14,9 +14,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,                         KC_Y, KC_U, KC_I,   KC_O,   KC_P,    KC_MINS,
         KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,                         KC_H, KC_J, KC_K,   KC_L,   KC_SCLN, KC_QUOT,
         KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                         KC_N, KC_M, KC_COMM,KC_DOT ,KC_SLSH, KC_BSLASH,
-                                 RAISE ,KC_SPC,                        LOWER,
-                                                KC_BSPC,                  KC_ENT,
-                                                                    KC_DEL
+                                 RAISE ,                               LOWER,
+                                     KC_SPC,                           KC_ENT,
+                                        KC_BSPC,                       KC_DEL
         ),
 
     [_LOWER] = LAYOUT_4x6(
@@ -24,9 +24,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
         _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
         _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
-                                      _______,KC_BTN1,                          _______,
-                                                      KC_BTN3,                     _______,
-                                                                             QK_BOOT
+                                              _______,                          _______,
+                                                  KC_BTN1,                      _______,
+                                                     KC_BTN3,                   QK_BOOT
 
         ),
 
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
         _______,KC_LEFT,KC_UP,KC_DOWN,KC_RGHT,KC_LPRN,                          KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
         _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
-                                       _______,_______,                         _______,
-                                                      QK_BOOT,                     _______,
-                                                                             _______
+                                              _______,                          _______,
+                                                   _______,                     _______,
+                                                        QK_BOOT,                _______
         )
 };
 

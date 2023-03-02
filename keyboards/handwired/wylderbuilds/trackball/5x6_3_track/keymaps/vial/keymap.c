056a9c592398717c504563bb08b99c109de2013b
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-03-02 18:16:03 -0500

    Added some mouse keys to raise and lower layers on 5x6_3_track

--- keyboards/handwired/wylderbuilds/trackball/5x6_3_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/5x6_3_track/keymaps/vial/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
         _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
         _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
-        _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+        _______,_______,_______,SNIPING,DRGSCRL,KC_BTN1,                     _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
                         _______,KC_PSCR,                                                         _______, KC_P0,
                                               _______,_______,_______,         _______,
                                                                                _______,
@@ -36,11 +36,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
         _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
         _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
-        _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
+        _______,_______,_______,SNIPING,DRGSCRL,KC_BTN1,                        _______,_______,_______,_______,_______,KC_VOLD,
                         _______,_______,                                                        KC_EQL ,_______,
                                              _______,_______,QK_BOOT,           _______,
                                                                                 _______,
-                                                                         _____,_______
+                                                                        _______,_______
         )
 };
 

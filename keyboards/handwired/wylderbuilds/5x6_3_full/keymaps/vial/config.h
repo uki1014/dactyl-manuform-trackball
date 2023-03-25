f1e7adbfab63c4caf604980c12a224350c3a3fee
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-03-25 19:38:33 -0400

    Added 5x6_3_full keyboard, like the Chimera

---
+++ keyboards/handwired/wylderbuilds/5x6_3_full/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xEA, 0x1F, 0xB2, 0x38, 0xA4, 0xE3, 0xD2, 0xC1}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 6 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 6 }

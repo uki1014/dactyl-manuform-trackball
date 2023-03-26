974edda5215929a5cbb6bdf57003c02029b8a08f
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-03-26 16:29:09 -0400

    Added 6x6_track

---
+++ keyboards/handwired/wylderbuilds/trackball/6x6_track/6x6_track.c
@@ -0,0 +1,1 @@
+#include "6x6_track.h"
---
+++ keyboards/handwired/wylderbuilds/trackball/6x6_track/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFF, 0x0C, 0xE7, 0x47, 0x22, 0x22, 0x4F, 0x9A}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 5 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }

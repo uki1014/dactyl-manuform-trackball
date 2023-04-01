fa3fa13e551d294da9a1c1fa5a8b2075f4c983f7
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-03-31 21:50:16 -0400

    Added 3x6_3

---
+++ keyboards/handwired/wylderbuilds/3x6_3/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xCE, 0xE8, 0x2F, 0xFC, 0x13, 0x08, 0xF2, 0x39}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }

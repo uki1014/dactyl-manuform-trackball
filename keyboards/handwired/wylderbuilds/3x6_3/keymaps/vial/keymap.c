37eefa2e32e7a1ee759038aa8de842f7082d3199
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-04-02 10:43:27 -0400

    Fixed bad formatting in keymap.c

--- keyboards/handwired/wylderbuilds/3x6_3/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/3x6_3/keymaps/vial/keymap.c
@@ -16,9 +16,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_3x6_3(
         KC_ESC , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                     KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
         KC_TAB,  KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                     KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-        KC_LSFT, KC_Z  , KC_X
-        , KC_C  , KC_V  , KC_B  ,                     KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
-                                    RAISE,   KC_SPC,  KC_TAB,      KC_LCTRL, KC_ENT, LOWER
+        KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                     KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
+                                    RAISE,   KC_SPC,  KC_TAB,          KC_LCTRL, KC_ENT, LOWER
         ),
     [_RAISE] = LAYOUT_split_3x6_3(
         _______,_______,_______,_______,_______,KC_LBRC,                    KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,

b4da9d55c98a77060ad9d213b1e7d2f5ade62f81
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-05-06 18:17:37 -0400

    3x5_3_track in a good place.

--- keyboards/handwired/wylderbuilds/trackball/3x5_3_track/config.h
+++ keyboards/handwired/wylderbuilds/trackball/3x5_3_track/config.h
@@ -1,48 +1,111 @@
-/*
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
-Copyright 2021 @dlford
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
+// Dactyl Manuform Hotswap
 #pragma once
 
 #include "config_common.h"
+// Basic Config
 
-// Communication
-// #define USE_I2C
+#ifndef WYLD_QMK_FIRMWARE_CONFIG_H
+#define WYLD_QMK_FIRMWARE_CONFIG_H
+
+#endif // WYLD_QMK_FIRMWARE_CONFIG_H
+#define BOOTMAGIC_LITE_ROW 0        // TOP RIGHT KEY ON RIGHT SIDE, TOP LEFT KEY ON LEFT SIDE
+#define BOOTMAGIC_LITE_COLUMN 4
+
+//#define PRODUCT_ID 0x0002
+
 #define USE_SERIAL
+#define SPLIT_HAND_PIN      GP26  // high = left, low = right
+//#define	SPLIT_HAND_PIN_LOW_IS_LEFT
+//#define SPLIT_HAND_MATRIX_GRID GP9, GP8
+//#define MASTER_RIGHT
+//#define MASTER_LEFT
+
+
+// Using Serial instead of I2C
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_TX_PIN GP0
+#define SERIAL_USART_RX_PIN GP1
+#define SERIAL_PIO_USE_PIO0
+#define SERIAL_USART_TIMEOUT     100  // USART driver timeout. default 100
+#define SERIAL_USART_SPEED 921600
+//#define SERIAL_USART_PIN_SWAP
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED_MASK 0U
 
 /* key matrix size */
-// Rows are doubled-up
+// Columns are doubled-up
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 5
 
-// wiring of each half
-#define MATRIX_COL_PINS { C6, D7, E6, B4, B5 }
-#define MATRIX_ROW_PINS { B1, B3, B2, B6 }
+#define MATRIX_COL_PINS { GP2, GP3, GP4, GP5, GP6 }
+#define MATRIX_ROW_PINS { GP9, GP10, GP11, GP12 }
 
 #define DIODE_DIRECTION COL2ROW
 
+#define SPLIT_POINTING_ENABLE
+#define ROTATIONAL_TRANSFORM_ANGLE  -25
+#define POINTING_DEVICE_INVERT_Y
+#define POINTING_DEVICE_RIGHT
+#define PMW33XX_CS_PIN GP21
+#define POINTING_DEVICE_TASK_THROTTLE_MS 1
+#define PMW33XX_LIFTOFF_DISTANCE 0x02
+
+#define SPLIT_TRANSACTION_IDS_KB RPC_ID_KB_CONFIG_SYNC
+
+//#define CHARYBDIS_MINIMUM_DEFAULT_DPI 1200
+//#define CHARYBDIS_DEFAULT_DPI_CONFIG_STEP 200
+//#define CHARYBDIS_MINIMUM_SNIPING_DPI 400
+//#define CHARYBDIS_SNIPING_DPI_CONFIG_STEP 200
+
+//// Rotary Encoder ////
+// GP21
+// GP22
+
+//// Sensor Notes ////
+//// Pi Pico pins ////
+// 3V3  -- Power (not 3v3_EN!)
+// GP21 -- CS pin
+// GP20 -- MISO pin
+// GP19 -- MOSI pin
+// GP18 -- SCK pin clock
+// GND  -- GND
+
 // WS2812 RGB LED strip input and number of LEDs
-//#define RGB_DI_PIN D3
-//#define DRIVER_LED_TOTAL 36
-//#define RGB_MATRIX_SPLIT { 18, 18 }
-//#define RGB_MATRIX_CENTER { 133, 54 }
+//#ifdef RGBLIGHT_ENABLE
+//#define RGB_DI_PIN GP17
+//#define RGBLED_NUM 34
+////#define DRIVER_LED_TOTAL 34
+//#define RGBLED_SPLIT { 17, 17 }
+//#define STM32_SYSCLK KINETIS_SYSCLK_FREQUENCY
+//#define NOP_FUDGE 0.4
+//#endif
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-//#undef LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#undef LOCKING_RESYNC_ENABLE
+//// OLED Pins and Driver
+#ifdef OLED_ENABLE
+#define I2C_DRIVER I2CD1
+#define I2C1_SDA_PIN GP16
+#define I2C1_SCL_PIN GP17
+// OLED Options
+#define SPLIT_OLED_ENABLE
+#define SPLIT_WPM_ENABLE
+#define OLED_DISPLAY_WIDTH 128
+#define OLED_DISPLAY_HEIGHT 32
+#define OLED_MATRIX_SIZE 512
+#define OLED_RESET -1
+#define OLED_DISPLAY_ADDRESS 0x3C
+#define OLED_BRIGHTNESS 255
+#define OLED_TIMEOUT 32000
+#define OLED_FADE_OUT
+#define OLED_FADE_OUT_INTERVAL 0
+#endif
 
-/* Enables This makes it easier for fast typists to use dual-function keys */
-#undef PERMISSIVE_HOLD
+// Misc settings
+// Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap
+#define LOCKING_SUPPORT_ENABLE
+// Locking resynchronize hack
+#define LOCKING_RESYNC_ENABLE
+// Enables This makes it easier for fast typists to use dual-function keys
+#define PERMISSIVE_HOLD
+// End of Basic Config
---
+++ keyboards/handwired/wylderbuilds/trackball/3x5_3_track/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0C, 0xE6, 0x63, 0x9D, 0xC8, 0xFC, 0xF8, 0x7D}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }

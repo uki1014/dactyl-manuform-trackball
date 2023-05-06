e78d5b2e303aff8c58b26ddcf5a5c225a25baed8
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-05-06 18:20:21 -0400

    Qwerty.  It should be qwerty.

--- keyboards/handwired/wylderbuilds/trackball/3x5_3_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/3x5_3_track/keymaps/vial/keymap.c
@@ -10,9 +10,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_split_3x5_3(
-        KC_Q  ,KC_W  , KC_F  , KC_P  , KC_G  ,                       KC_J  , KC_L  , KC_U  , KC_Y  , KC_SCLN,
-        KC_A  , KC_R  , KC_S  , KC_T  , KC_D  ,                      KC_H  , KC_N  , KC_E  , KC_I  , KC_O,
-        KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                      KC_K  , KC_M  ,KC_COMM,KC_DOT , KC_SLSH,
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
                                       RAISE, KC_SPC, KC_BSPC,      LOWER,
                                                                    KC_ENT,
                                                                 KC_LALT

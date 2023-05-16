bb663bd13e2dbec0898f19aee4443581b3c247ac
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-05-16 12:00:07 -0400

    Back to standard 5x6_track

--- keyboards/handwired/wylderbuilds/trackball/5x6_track/rules.mk
+++ keyboards/handwired/wylderbuilds/trackball/5x6_track/rules.mk
@@ -9,9 +9,9 @@ CONSOLE_ENABLE = no
 BOOTMAGIC_ENABLE = yes
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 NKRO_ENABLE = yes           # Enable N-Key Rollover
-OLED_ENABLE = yes
-WPM_ENABLE = yes
-ENCODER_ENABLE = yes
+OLED_ENABLE = NO
+WPM_ENABLE = NO
+ENCODER_ENABLE = NO
 ENCODER_MAP_ENABLE = no
 
-OPT_DEFS += -DHAL_USE_I2C=TRUE
+#OPT_DEFS += -DHAL_USE_I2C=TRUE

3a7ec511f24f7267c429330efcbb0275c8b366d2
bullwinkle3000 <91431084+bullwinkle3000@users.noreply.github.com>
2023-05-16 20:06:21 -0400

    workman on 5x6

--- keyboards/handwired/wylderbuilds/trackball/5x6_track/keymaps/vial/keymap.c
+++ keyboards/handwired/wylderbuilds/trackball/5x6_track/keymaps/vial/keymap.c
@@ -1,14 +1,27 @@
 #include QMK_KEYBOARD_H
 #include "5x6_track.h"
 
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
+#define _WORKMAN 0
+#define _QWERTY 1
+#define _LOWER 2
+#define _RAISE 3
 
+#define QWERTY MO(_QWERTY)
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_WORKMAN] = LAYOUT_5x6(
+        KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                      KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
+        KC_TAB , KC_Q  , KC_D  , KC_R  , KC_W  , KC_B  ,                      KC_J  , KC_F  , KC_U  , KC_P  , KC_SCLN,KC_MINS,
+        KC_LSFT, KC_A  , KC_S  , KC_H  , KC_T  , KC_G  ,                      KC_Y  , KC_N  , KC_E  , KC_O  , KC_I , KC_QUOT,
+        KC_LCTL, KC_Z  , KC_X  , KC_M  , KC_C  , KC_V  ,                      KC_K  , KC_L  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
+                         KC_LBRC,KC_RBRC,                                                    KC_PLUS, KC_EQL,
+                                                 RAISE, KC_SPC,               LOWER,
+                                                 KC_TAB, KC_BSPC,             KC_ENT,
+                                                 QWERTY, KC_GRV,     KC_DEL, KC_LALT
+        ),
+
     [_QWERTY] = LAYOUT_5x6(
         KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                      KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
         KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                      KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
@@ -16,8 +29,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                      KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
                          KC_LBRC,KC_RBRC,                                                    KC_PLUS, KC_EQL,
                                                  RAISE, KC_SPC,               LOWER,
-                                                 KC_TAB, KC_HOME,             KC_ENT,
-                                                 KC_BSPC, KC_GRV,     KC_DEL, KC_LALT
+                                                 KC_TAB, KC_BSPC,             KC_ENT,
+                                                 KC_HOME, KC_GRV,     KC_DEL, KC_LALT
         ),
 
     [_LOWER] = LAYOUT_5x6(
